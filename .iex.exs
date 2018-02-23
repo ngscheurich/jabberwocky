b30c40edf396b0ef2d97f1da73bf3a0c06fc602a
Nicholas Scheurich <nick@scheurich.me>
2018-02-23 17:36:20 -0600

    Add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,7 @@
+alias Jabberwocky.ServiceRequests
+alias Jabberwocky.ServiceRequests.ServiceRequest
+alias Jabberwocky.Users
+alias Jabberwocky.Users.User
+alias Jabberwocky.Mailer
+alias Jabberwocky.Email
+alias Jabberwocky.Repo
