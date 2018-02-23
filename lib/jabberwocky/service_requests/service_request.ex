a0500eac30c825b2dd2e999fcb03bc908201fb01
Nicholas Scheurich <nick@scheurich.me>
2018-02-23 12:14:17 -0600

    Add service request schema

---
+++ lib/jabberwocky/service_requests/service_request.ex
@@ -0,0 +1,33 @@
+defmodule Jabberwocky.ServiceRequest do
+  @defmodule """
+  Represents a service request filed with the city, along with 
+  some additional data we need for tracking progress and sending
+  status updates.
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias __MODULE__
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  
+  schema "service_requests" do
+    field :address, :string
+    field :type, ServiceRequestTypeEnum
+    field :notify, :boolean, default: false
+    field :image_url, :string
+    field :service_request_id, :string
+    field :status, ServiceRequestStatusEnum
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%ServiceRequest{} = service_request, attrs) do
+    service_request
+    |> cast(attrs, [:address, :type, :notify, :image_url, :service_request_id, :status])
+  end
+end
