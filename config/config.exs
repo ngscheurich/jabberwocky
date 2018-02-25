e764e7c9123f7fff9cdc2dff0623bf84d6c45caa
Nicholas Scheurich <nick@scheurich.me>
2018-02-25 01:26:46 -0600

    Formatting

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :jabberwocky,
-  ecto_repos: [Jabberwocky.Repo]
+config :jabberwocky, ecto_repos: [Jabberwocky.Repo]
 
 # Configures the endpoint
 config :jabberwocky, JabberwockyWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "f2H5y4GslhyIlpIIWH1ah/3ysey25WFFfLcNkRGw/guQUAG3/YKVLf5nP9me44zr",
   render_errors: [view: JabberwockyWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: Jabberwocky.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Jabberwocky.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -27,4 +25,4 @@ config :jabberwocky, :email_client, Jabberwocky.Email.Mailer
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
