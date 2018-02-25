2239dc13fc92b011f3c1f1fd88107244105f3bb6
Nicholas Scheurich <nick@scheurich.me>
2018-02-25 01:47:54 -0600

    Fix callback parameter type

--- lib/jabberwocky/email/client.ex
+++ lib/jabberwocky/email/client.ex
@@ -1,5 +1,3 @@
 defmodule Jabberwocky.Email.Client do
-  alias Jabberwocky.BR311.ServiceRequest
-
-  @callback deliver_later(%ServiceRequest{}) :: %Bamboo.Email{}
+  @callback deliver_later(%Bamboo.Email{}) :: %Bamboo.Email{}
 end
